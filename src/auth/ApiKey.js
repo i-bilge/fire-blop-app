790c987be75374826a92ad02187a9bcf07d00410
i-bilge <iismailbilge@gmail.com>
2022-08-15 12:13:27 +0200

    Api key added

---
+++ src/auth/ApiKey.js
@@ -0,0 +1,11 @@
+export const API_KEY = "5e5ba60b460ceadbd75b3baff60a87de";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDlrS_ksXb_WCGY_TieWq1T4Qsv4SMIRqw",
+  authDomain: "blog-app-7f248.firebaseapp.com",
+  projectId: "blog-app-7f248",
+  storageBucket: "blog-app-7f248.appspot.com",
+  messagingSenderId: "112977480179",
+  appId: "1:112977480179:web:06cae668cb94c04cfc6a86"
+};
+  
